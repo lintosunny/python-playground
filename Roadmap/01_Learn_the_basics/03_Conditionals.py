abc1d09f27b6997dd6ae9dc20cbf2fbfe4612967
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-06-13 17:21:41 +0530

    Create 03_Conditionals.py

---
+++ Roadmap/01_Learn_the_basics/03_Conditionals.py
@@ -0,0 +1,35 @@
+# control structure directs the order of execution of the statements in a program
+if country == "India":
+    if state == "Kerala":
+        print("India-Kerala")
+else:
+    print("India-Other")
+
+
+# switch case statement
+# A switch statement is a multiway branch statement that compares the value of a variable to the values specified in case statements.
+# Python language doesn’t have a switch statement.
+# Python uses dictionary mapping to implement Switch Case in Python
+def switchExample(arg):
+    switcher =  {
+        0: "This is case zero",
+        1: "This is case one",
+        2: "This is case two"
+    }
+    return switcher.get(argument, "nothing")
+
+if __name__ == "__main__":
+    arg = 1
+    print(switchExample(arg))
+
+
+# match case statement
+# structural pattern matching 
+command = "Hello, world"
+match command:
+    case "Hello, world":
+        print("Hello")
+    case "Goodbye, world":
+        print("Bye")
+    case other:
+        print("No match")
