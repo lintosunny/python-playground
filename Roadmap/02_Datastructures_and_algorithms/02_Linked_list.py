00cd0df55a55303e65b8614256541813325daa8f
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-06-15 17:43:35 +0530

    Create 02_Linked_list.py

---
+++ Roadmap/02_Datastructures_and_algorithms/02_Linked_list.py
@@ -0,0 +1,10 @@
+# lesser known cousin of list
+# ordered collection of objects
+# store references as part of their own elements
+# Each element of a linked list is called a node;
+#     1. Data: value to be stored in the node
+#     2. Next: reference to the next node
+
+# usecases;
+# 1. implement queues, or stacks as well as graphs
+# 2. lifecycle management of an OS
