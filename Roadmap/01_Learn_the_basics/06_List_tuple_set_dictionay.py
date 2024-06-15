d8c4cbdeaf7960c941f36cdc3c1d79df30703ec1
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-06-15 15:36:38 +0530

    Create 06_List_tuple_set_dictionay.py

---
+++ Roadmap/01_Learn_the_basics/06_List_tuple_set_dictionay.py
@@ -0,0 +1,35 @@
+# four in-built data types in python are;
+
+# 1. Lists
+# Dynamic sized arrays
+# hetrogeneous
+list1 = ["A", "B", 4, 3]
+
+# 2. Tuple
+# collection of objects separated by comma
+# similar to list but immutable
+tuple1 = ("A", "B", 4, 3)
+
+# 3. Set
+# Unordered collection data type
+# iterable, mutable, and have no duplicate elements
+set1 = (["A", "B", 4, 3])
+
+# 4. Dictionary
+# ordered (since python 3.7) collection of values
+# store data values like a map
+# Dictionary holds key-value pair
+dic1 = {
+  3: "A",
+  4: "B"
+}
+
+# +---------------------+--------------------+----------------+-----------------+
+# | Tuples              | Lists              | Sets           | Dictionary      |
+# +---------------------+--------------------+----------------+-----------------+
+# | Duplicates          | Duplicates         | No duplicates  | No duplicates   |
+# | Ordered             | Ordered            | Unordered      | Unordered       |
+# | Mutable             | Mutable            | Immutable      | Mutable         |
+# | Indexing & slicing  | Indexing & slicing | -              | -               |             
+# | faster than list    |                    |                |                 |
+# +---------------------+--------------------+----------------+-----------------+
