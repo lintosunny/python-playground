c52fb233d251543e5dcc629a1f2927389062b184
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-06-24 12:29:55 +0530

    Update 03_Heaps.py

--- Roadmap/02_Datastructures_and_algorithms/03_Heaps.py
+++ Roadmap/02_Datastructures_and_algorithms/03_Heaps.py
@@ -1,1 +1,5 @@
-#
+# A tree-based data structure in which the value of a parent node is ordered in a certain way with respect to the value of its child node(s). 
+# A heap can be either a min heap (the value of a parent node is less than or equal to the value of its children) or 
+# a max heap (the value of a parent node is greater than or equal to the value of its children).
+
+# Python’s heap is a min-heap, its first element always has the lowest value.
