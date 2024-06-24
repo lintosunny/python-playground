9e8242e944cf8fbbd80154e163aa8de0a1672f7e
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-06-24 11:57:22 +0530

    Update 05_Queue.py

--- Roadmap/02_Datastructures_and_algorithms/05_Queue.py
+++ Roadmap/02_Datastructures_and_algorithms/05_Queue.py
@@ -43,3 +43,24 @@ q.full()  # False
 q.get()
 q.get()
 q.empty()  # True
+
+
+# custom class
+from collections import deque
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.deque()
+          
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
