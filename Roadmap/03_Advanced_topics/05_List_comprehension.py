0232ebe590f0b79d581101acaa58835df4e40915
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-07-06 09:46:56 +0530

    Update 05_List_comprehension.py

--- Roadmap/03_Advanced_topics/05_List_comprehension.py
+++ Roadmap/03_Advanced_topics/05_List_comprehension.py
@@ -15,3 +15,16 @@ fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = ['hello' for x in fruits]
 print(newlist)  #-> ['hello', 'hello', 'hello', 'hello', 'hello']
 
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x if x != "banana" else "orange" for x in fruits]
+print(newlist)  #-> ['apple', 'orange', 'cherry', 'kiwi', 'mango']
+
+newlist = [0 if i % 2 == 0 else i for i in range(10)]
+print(newlist)  #-> [0, 1, 0, 3, 0, 5, 0, 7, 0, 9]
+
+s = 'Bye'
+newlist = [' ' + char.upper() + ' ' for char in s]
+print(newlist)  #-> [' B ', ' Y ', ' E ']
+
+cordinates = [(i, j) for i in range(3) for j in range(2)]
+print(cordinates)  #-> [(0, 0), (0, 1), (1, 0), (1, 1), (2, 0), (2, 1)]
