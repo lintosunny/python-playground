63e58142bb67ed365e63cd8c4be3204a6240063c
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-07-06 17:16:31 +0530

    Create 07_Paradigms.py

---
+++ Roadmap/03_Advanced_topics/07_Paradigms.py
@@ -0,0 +1,20 @@
+# Python is a multi-paradigm programming language
+# means support several paradigms
+
+
+# 1.Imperative programming
+# focuses on telling computer what to do, step by step
+# variables, loops, control structures
+
+# 2.Object-oriented Programming (OOP)
+# Based on the idea of object and their interactions
+# class, inheritance, polymorphism
+
+# 3.Functional programming
+# Base on the idea of functions as first-class citizens
+# emphasis the use of pure functions and immutable data
+# higher order functions, lambda, generators
+
+# 4.Aspect-oriented programming 
+# Based on the idea of separating cross-cutting concerns from the main functionality of a program
+# Don't have this as built in feature. Have to use libraries
