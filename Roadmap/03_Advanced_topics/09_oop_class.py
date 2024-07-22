037501608964238e777409d2963bbe66632e9a88
Linto N S <119283666+lintosunny@users.noreply.github.com>
2024-07-22 17:40:34 +0530

    Create 09_oop_class.py

---
+++ Roadmap/03_Advanced_topics/09_oop_class.py
@@ -0,0 +1,4 @@
+# Class is user defined blueprint from which objects are created.
+# Class provides a means of bundling data and functionality together.
+# Each class instance can have attributes attached to it for maintaining its state.
+# Instance can have methods for modifying their state.
